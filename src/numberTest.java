cd7c26c7397792c370ed51c85388f3d9e14d5730
ogu10 <t_ogura+4@jobins.jp>
2022-06-16 18:10:44 +0900

    save 20220616_18:10

---
+++ src/numberTest.java
@@ -0,0 +1,10 @@
+public class numberTest {
+    int x =5;
+
+    public static void main(String[] args) {
+        numberTest test1 = new numberTest();
+        numberTest test2 = new numberTest();
+        System.out.println(test1.x);
+        System.out.println(test2.x);
+    }
+}
