644fb75723d34fee59e4079385f9e00ed48fb5e3
ogu10 <t_ogura+4@jobins.jp>
2022-06-16 18:18:02 +0900

    save 20220616_18:18

--- src/numberTest.java
+++ src/numberTest.java
@@ -1,9 +1,9 @@
-public class numberTest {
+public class NumberTest {
     int x =5;
 
     public static void main(String[] args) {
-        numberTest test1 = new numberTest();
-        numberTest test2 = new numberTest();
+        NumberTest test1 = new NumberTest();
+        NumberTest test2 = new NumberTest();
         System.out.println(test1.x);
         System.out.println(test2.x);
     }
