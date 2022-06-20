e9473f1b4b3994bb9e89c41d107939f8ac09ff7c
ogu10 <t_ogura+4@jobins.jp>
2022-06-20 17:07:52 +0900

    save 20220620_17:07

--- src/Test1_setupTrial/InitialTest.java
+++
@@ -1,7 +0,0 @@
-public class InitialTest {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-    }
-
-}
--- src/Test1_setupTrial/Second.java
+++
@@ -1,7 +0,0 @@
-class Second {
-    public static void main(String[] args) {
-        Main myCar = new Main();
-        myCar.fullThrottle();
-        myCar.speed(200);
-    }
-}
