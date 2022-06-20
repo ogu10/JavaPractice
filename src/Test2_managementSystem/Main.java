9d94feb1bb2f5f8144f26427b0241fe30afa3257
ogu10 <t_ogura+4@jobins.jp>
2022-06-20 16:21:41 +0900

    save 20220620_16:21

---
+++ src/Test2_managementSystem/Main.java
@@ -0,0 +1,10 @@
+public class NumberTest {
+    int x =5;
+
+    public static void main(String[] args) {
+        NumberTest test1 = new NumberTest();
+        NumberTest test2 = new NumberTest();
+        System.out.println(test1.x);
+        System.out.println(test2.x);
+    }
+}
