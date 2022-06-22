0c71dd20079721bfd12587e6e7b0e3d54651f92c
ogu10 <t_ogura+4@jobins.jp>
2022-06-22 18:39:11 +0900

    save 20220622_18:39

--- src/Test2_managementSystem/Scores.java
+++ src/Test2_managementSystem/Scores.java
@@ -8,10 +8,10 @@ public class Scores {
         System.out.println("\r");
         System.out.println("Input Scores of a new Student.");
         Scanner nameInput = new Scanner(System.in);
-        Scanner mathInput = new Scanner(System.in);
-        Scanner chemistryInput = new Scanner(System.in);
-        Scanner physicsInput = new Scanner(System.in);
         int count = 1;
+        int math;
+        int chemistry;
+        int physics;
 
         while (true) {
             System.out.println("Type name of no." + count + " student!");
@@ -19,50 +19,36 @@ public class Scores {
             String name = nameInput.nextLine();
             if ("q".equals(name)) break;
 
+            math = Scores.inputFunc("math");
+            chemistry = Scores.inputFunc("chemistry");
+            physics = Scores.inputFunc("physics");
 
-            try {
-                System.out.println("Input Math score!");
-                int math = mathInput.nextInt();
+            System.out.println("\r");
 
-                System.out.println("Input Chemistry score!");
-                int chemistry = chemistryInput.nextInt();
+            int i;
+            i = 2;
+            if (math + chemistry + physics < 180) i = -1;
+            if (math + chemistry + physics == 180) i = 0;
+            if (math + chemistry + physics > 180) i = 1;
 
-                System.out.println("Input Physics score!");
-                int physics = physicsInput.nextInt();
-
-                System.out.println("\r");
-
-                int i;
-                i = 2;
-                if (math + chemistry + physics < 180) i = -1;
-                if (math + chemistry + physics == 180) i = 0;
-                if (math + chemistry + physics > 180) i = 1;
-
-                switch (i) {
-                    case -1:
-                        System.out.println("\"" + name + "\"" + " has... \u001b[00;41mFailed.\u001b[00m");
-                        break;
-                    case 0:
-                        System.out.println("\"" + name + "\"" + " has... \u001b[00;46mSurvived!\u001b[00m");
-                        break;
-                    case 1:
-                        System.out.println("\"" + name + "\"" + " has... \u001b[00;42mPassed!!\u001b[00m");
-                        break;
-                    default:
-                        System.out.println("Invalid data");
-                }
-
-                System.out.println("[Math] " + math);
-                System.out.println("[Chemistry] " + chemistry);
-                System.out.println("[Physics] " + physics);
-                System.out.println("[Average] " + (math + chemistry + physics) / 3 + " (qualified by 60 points)");
-
-            } catch (InputMismatchException e) {
-                System.out.println("\r");
-                System.out.println("\u001b[00;31m--Suuji de Tanomu--\u001b[00m");
-                /*System.out.println(e);*/
-                break;
+            switch (i) {
+                case -1:
+                    System.out.println("\"" + name + "\"" + " has... \u001b[00;41mFailed.\u001b[00m");
+                    break;
+                case 0:
+                    System.out.println("\"" + name + "\"" + " has... \u001b[00;46mSurvived!\u001b[00m");
+                    break;
+                case 1:
+                    System.out.println("\"" + name + "\"" + " has... \u001b[00;42mPassed!!\u001b[00m");
+                    break;
+                default:
+                    System.out.println("Invalid data");
             }
+
+            System.out.println("[Math] " + math);
+            System.out.println("[Chemistry] " + chemistry);
+            System.out.println("[Physics] " + physics);
+            System.out.println("[Average] " + (math + chemistry + physics) / 3 + " (qualified by 60 points)");
 
             count++;
             System.out.println("\r");
@@ -72,4 +58,18 @@ public class Scores {
         System.out.println("\r");
         System.out.println("--bye bye--");
     }
+
+    public static int inputFunc(String fname) {
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Input " + fname + " score!");
+            return input.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("use number please...");
+            Scores.inputFunc(fname);
+        }
+
+        return 0;
+    }
+
 }
