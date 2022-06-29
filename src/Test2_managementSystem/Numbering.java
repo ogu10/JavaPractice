df63322c1e71999eb57e72925f8b70198aae6fd6
ogu10 <t_ogura+4@jobins.jp>
2022-06-29 10:46:27 +0900

    save 20220629_10:46

--- src/Test2_managementSystem/Numbering.java
+++ src/Test2_managementSystem/Numbering.java
@@ -3,8 +3,27 @@ package Test2_managementSystem;
 public class Numbering {
     public static void main(String[] args) {
 
+        //read 5 numbers of users input and insert it into variables "number1" ~ "number5"
+            //message "type 1st number"
+            //message "type 2nd one"
+            //
+            //
+            //
+        //add these numbers into an array
+
+        //if loop to check smallest number of array
+            //check one by one and if new one is smaller, override "smallestNumber", and after checking all, insert it into "smallest1"
+
+        //remove smallest one
+
+        //if loop to check second smallest, and insert it into "smallest2", and remove it from the array
+        //if loop to check third smallest
+        //if loop to check fourth smallest
+        //if loop to check fifth smallest
 
 
+        //add these into an array
+        //print it out
 
 
 
---
+++ src/Test2_managementSystem/Searching.java
@@ -0,0 +1,15 @@
+package Test2_managementSystem;
+
+public class Searching {
+    public static void main(String[] args){
+
+        //read 5 numbers of users input and insert it into variables "number1" ~ "number5"
+
+        //add these numbers into an array
+
+        //if loop to check smallest number of array
+            //check one by one and if new one is smaller, override "smallestOrder", and after checking all, print the order
+
+
+    }
+}
