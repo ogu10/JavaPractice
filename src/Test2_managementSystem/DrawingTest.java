258140cd86cd66a23ee40efbb0dca7522b229ee1
ogu10 <t_ogura+4@jobins.jp>
2022-07-05 14:05:03 +0900

    save 20220705_14:04

---
+++ src/Test2_managementSystem/DrawingTest.java
@@ -0,0 +1,19 @@
+package Test2_managementSystem;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DrawingTest extends JFrame implements ActionListener {
+    JButton mybtn;
+    JPanel mypnl;
+    public DrawingTest() {
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource() == mybtn){
+
+        }
+    }
+}
