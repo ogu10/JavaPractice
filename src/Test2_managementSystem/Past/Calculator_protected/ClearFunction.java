03660ed8402de1fdf28d263128d41129ec62d9a5
ogu10 <t_ogura+4@jobins.jp>
2022-07-05 12:55:17 +0900

    save 20220705_12:55

--- src/Test2_managementSystem/Past/Calculator_protected/ClearFunction.java
+++
@@ -1,9 +0,0 @@
-package Test2_managementSystem.Calculator;
-
-public class ClearFunction {
-    public static void Clear(){
-        MyCalculator.label.setText("");
-        MyCalculator.label2.setText(Integer.toString(0));
-        MyCalculator.label3.setText("");
-    }
-}
