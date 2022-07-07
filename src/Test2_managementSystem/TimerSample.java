65f876998212a7d3205d3220b3a7840df8330c74
ogu10 <t_ogura+4@jobins.jp>
2022-07-07 19:28:18 +0900

    save 20220707_19:28

---
+++ src/Test2_managementSystem/TimerSample.java
@@ -0,0 +1,31 @@
+package Test2_managementSystem;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerSample{
+    public int i = 0;
+    public static void main(String[] args) {
+        new TimerSample();
+    }
+    public TimerSample(){
+        TimeStar star = new TimeStar(); //タイマータスクインスタンス作成
+        Timer timer = new Timer(); //タイマーインスタンス作成
+        System.out.println( "after 1sec");
+        System.out.println( "show * by 1sec");
+        //タイマー開始　5秒待って1秒ごとにタイマータスクstarを繰り返し
+        timer.schedule(star, 1000,1000);
+
+    }
+    //TimerTask内部クラス
+    class TimeStar extends TimerTask {
+        public void run() {
+            System.out.print("*");
+            i++;
+            if(i%10==0){
+                System.out.println();
+            }
+            if(i==20) System.exit(0);
+        }
+    }
+}
