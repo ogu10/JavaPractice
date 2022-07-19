7e342705285c20a12aca727468e252a37ccf933c
ogu10 <t_ogura+4@jobins.jp>
2022-07-19 19:20:27 +0900

    save 20220719_19:20

--- src/Test2_managementSystem/GameWindow.java
+++ src/Test2_managementSystem/GameWindow.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.io.*;
 
 public class GameWindow  extends JFrame {
     JLabel label;
@@ -23,13 +23,54 @@ public class GameWindow  extends JFrame {
     }
 
     GameWindow() {
+
+        BufferedReader br = null;
+        String file_name = "C:\\Users\\ogura.MYCOMPUTER\\IdeaProjects\\JavaPractice\\20-20-Array.txt";
+        final int MAX_DATA_NUMS = 20; //まあここはなんでも
+        String[][] data = new String[MAX_DATA_NUMS][MAX_DATA_NUMS];
+
+        try {
+            File file = new File(file_name);
+            br = new BufferedReader(new FileReader(file));
+
+            for (int i=0;i<MAX_DATA_NUMS;i++) {
+                int c;
+                for (int j = 0; j < MAX_DATA_NUMS; j++) {
+                    c = br.read();
+                    while (c == 10 || c == 13) {
+                        c = br.read();
+                    }
+
+                    if (c != -1) {
+                        /*System.out.print((char) c);*/
+                        data[i][j] = String.valueOf(((char) c));
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                br.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
         setLayout(new GridLayout(20, 20, 0, 0));
         for(int x=0; x < 20; x++){
             for(int y=0; y < 20; y++){
-                String column = "a";
-                GridData.loading();
-                if (GridData.data[x][y] == "@"){column = "@";}
-                if (GridData.data[x][y] == "$"){column = "$";}
+                String column;
+                if (data[x][y] .equals("@")){column = "@";}
+                else if (data[x][y] .equals("$")){column = "$";}
+                else {column = "-";}
                 add(new JLabel(column));
             }
         }
